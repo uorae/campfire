13ad3f9b82048e5c2253412fe9011178ba0722f4
Rachel G <rachelg2273@gmail.com>
2025-12-29 00:27:57 +1100

    added buttons to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,29 @@
+import { useNavigate } from "react-router-dom"
+
 function Home() {
+    const navigate = useNavigate()
+
+    const openInventory = (e) => {
+        navigate("\inventory")
+    }
   return (
     <>
+        <div>
+            <button>
+                cookbook
+            </button>
+            <button onClick={() => navigate("\inventory")}>
+                pantry
+            </button>
+            <button>
+                shopping list
+            </button>
+        </div>
+        <div>
+            <button>
+                generate recipes
+            </button>
+        </div>
     </>
   )
 }
