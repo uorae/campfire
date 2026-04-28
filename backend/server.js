ae70c7a6d0a4ca6822a4a326494dac627d8db76b
Rachel G <rachelg2273@gmail.com>
2026-04-28 23:21:08 +1000

    deployment: update cors origin

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ const pool = new Pool({
 });
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://campfirecosy.netlify.app'
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
