c48bf28e5c0afc99a7759673b1a9bb25a0d00568
Rachel G <rachelg2273@gmail.com>
2026-04-29 17:09:18 +1000

    deployment: fixed port bug

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cors({
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
 
 /*****************************************************
  *                   PANTRY ROUTES                   *
