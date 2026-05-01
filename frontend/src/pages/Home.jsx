27325d0008b9a7bd3a25f24f810009e79ea61f57
Rachel G <rachelg2273@gmail.com>
2026-05-01 19:50:42 +1000

    update: updated landing page frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,28 +1,38 @@
 import { useNavigate } from 'react-router-dom'
+import NavBar from '../components/NavBar'
 
 function Home() {
 	const navigate = useNavigate()
 
 	return (
-		<div>
-			<h1 className="text-5xl dm-mono-medium font-bold">campfire .</h1>
-			<p className="text-lg text-base-content/60">look for a cozy meal</p>
+		<>
+			<div className="flex flex-col items-center h-screen">
+				<NavBar />
 
-			<div className="flex flex-row gap-4 mt-8">
-				<button
-					className="btn btn-primary btn-lg"
-					onClick={() => navigate('/inventory')}
-				>
-					inventory
-				</button>
-				<button
-					className="btn btn-secondary btn-lg"
-					onClick={() => navigate('/cookbook')}
-				>
-					cookbook
-				</button>
+				<div className="flex flex-col w-3/4 h-3/4 items-center pt-6">
+					{/* title */}
+					<div className="flex-1 justify-center items-center flex flex-col gap-12">
+						<h1 className="text-5xl lg:text-9xl md:text-8xl font-bold bonbon-regular">campfire</h1>
+						<p className="lg:text-3xl">look for a cozy meal</p>
+					</div>
+
+
+					{/* buttons */}
+					<div className="flex-1 flex flex-row justify-center items-center gap-8 md:gap-16 lg:gap-24">
+						<div className="cursor-pointer flex flex-col items-center" onClick={() => navigate('/inventory')}>
+							<img src="/assets/icons/inventory.png" alt="inventory icon" className="w-64 h-64 object-contain mb-2" />
+							<p className="text-center text-base-content/60">what's in your kitchen?</p>
+						</div>
+						<div className="cursor-pointer flex flex-col items-center" onClick={() => navigate('/cookbook')}>
+							<img src="/assets/icons/cookbook.png" alt="cookbook icon" className="w-64 h-64 object-contain mb-2" />
+							<p className="text-center text-base-content/60">find recipes!</p>
+						</div>
+					</div>
+				</div>
+
 			</div>
-		</div>
+
+		</>
 	)
 }
 
